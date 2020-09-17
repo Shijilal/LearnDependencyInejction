b51278be9faadd507c26fdd96657ff3e77646b4b
Shijilal <shijilal@hotmail.com>
2020-09-17 19:20:23 +0530

    Done

--- app/src/main/java/com/lalsoft/learndependencyinjection/MainActivity.kt
+++ app/src/main/java/com/lalsoft/learndependencyinjection/MainActivity.kt
@@ -5,20 +5,32 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var someClass: SomeClass
+    lateinit var someOtherClass: SomeOtherClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initializing the classes
+        someClass = SomeClass()
+        someOtherClass = SomeOtherClass()
+
+        // Getting the functions inside the classes
+        println(someClass.doAThing())
+        println(someOtherClass.doSomeOtherThing())
+
     }
 }
 
-class SomeClass(){
-    fun doAThing():String{
+class SomeClass() {
+    fun doAThing(): String {
         return "Look i did a thing!!"
     }
 }
 
-class SomeOtherClass(){
-    fun doSomeOtherThing():String{
+class SomeOtherClass() {
+    fun doSomeOtherThing(): String {
         return "Look i did some other thing!!"
     }
 }
