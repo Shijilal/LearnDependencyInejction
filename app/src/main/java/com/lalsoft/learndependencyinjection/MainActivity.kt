77b1010d23c1d40f971b3adf436db8951713740a
Shijilal <shijilal@hotmail.com>
2020-09-19 08:36:44 +0530

    Done

--- app/src/main/java/com/lalsoft/learndependencyinjection/MainActivity.kt
+++ app/src/main/java/com/lalsoft/learndependencyinjection/MainActivity.kt
@@ -11,15 +11,10 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var first: First
-    lateinit var second: Second
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Initializing the classes
-        second = Second()
-
 
         // Getting the functions inside the classes
         println(first.doAThing())
